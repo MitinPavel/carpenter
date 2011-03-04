7d9056dcaf63f210bdf4eb098ae4a790937eb1f9
Pavel Mitin <mitin.pavel@gmail.com>
2011-03-04 23:29:12 +0200

    RSpec is used in the project.

---
+++ spec/foobar_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+describe Object do
+  pending "write it"
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'carpenter'
+
+RSpec.configure do |config|
+end
