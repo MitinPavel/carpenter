0d9bd56f41b3fac7a90c6ed02beba935f7291e16
Pavel Mitin <mitin.pavel@gmail.com>
2011-03-05 08:50:46 +0200

    'with_' and 'for_' prefixed setters are created if .contains is called.

--- lib/carpenter/builder.rb
+++ lib/carpenter/builder.rb
@@ -12,8 +12,19 @@ module Carpenter
       def contains(*parts)
         parts.each do |part|
           attr_accessor part
+          define_setters part
         end
       end
+
+      def define_setters(part_name)
+        setter_name = "for_#{part_name}"
+        define_method setter_name do |part|
+          instance_variable_set "@#{part_name}", part
+          self
+        end
+        alias_method "with_#{part_name}", setter_name
+      end
+
     end
   end
 end
--- spec/contains_spec.rb
+++ spec/contains_spec.rb
@@ -24,5 +24,38 @@ describe Carpenter::Builder do
       builder.address = address
       builder.address.should == address
     end
+
+    it "should receive several arguments" do
+      user_builder_class.contains :name, :email
+    
+      builder = user_builder_class.new
+
+      builder.name = 'John'
+      builder.name.should == 'John'
+      builder.name = 'some@gmail.com'
+      builder.name.should == 'some@gmail.com'
+    end
+
+    context "'with_' and 'for_' prefixed setters" do
+      it "should add setters for the passed part" do
+        user_builder_class.contains :name
+        builder = user_builder_class.new
+
+        builder.with_name 'John'
+        builder.name.should == 'John'
+
+        builder.for_name 'Jill'
+        builder.name.should == 'Jill'
+      end
+
+      it "should allow to build a chain of calls" do
+        user_builder_class.contains :name, :email
+        builder = user_builder_class.new
+        builder.with_name('John').and.for_email('some@gmail.com')
+
+        builder.email.should == 'some@gmail.com'
+        builder.name.should == 'John'
+      end
+    end
   end
 end
