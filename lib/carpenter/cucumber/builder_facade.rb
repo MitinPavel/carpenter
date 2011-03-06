d69c4824db8c7427577e044721d37002a0474a44
Pavel Mitin <mitin.pavel@gmail.com>
2011-03-06 13:07:06 +0200

    The cucumber builder facade class works with builder attributes, passed through cucumber tables.

--- lib/carpenter/cucumber/builder_facade.rb
+++ lib/carpenter/cucumber/builder_facade.rb
@@ -1,6 +1,41 @@
 module Carpenter
   module Cucumber
     class BuilderFacade
+
+      def build(table)
+        table.hashes.each do |row|
+          row.each_pair do |key, value|
+            method, argument = RowParser.new.parse key, value
+            @builder.send method, argument
+          end
+        end
+
+        @builder.build!
+      end
+
+      private
+
+      def initialize(builder)
+        @builder = builder
+      end
+
+      class RowParser
+        def parse(key, value)
+          [process_key(key), process_value(value)]
+        end
+
+        private
+
+        def process_key(key)
+          attribute = key.gsub(/\s+/, '_').downcase
+
+          "with_#{attribute}"
+        end
+
+        def process_value(value)
+          value
+        end
+      end
     end
   end
 end
--- spec/carpenter/cucumber/builder_facade_spec.rb
+++ spec/carpenter/cucumber/builder_facade_spec.rb
@@ -2,17 +2,50 @@ require 'spec_helper'
 require 'lib/carpenter/cucumber/builder_facade'
 require 'cucumber/ast/table'
 
-describe Carpenter::Cucumber::BuilderFacade do
-  it "should just work" do
-    user_table = create_table <<-EOS
-      | Email  | Firstname |
-      | email1 | John      |
-      | email2 | Jill      |
-    EOS
+class User
+  attr_accessor :email, :first_name
 
-    user_table.hashes.each do |h|
-      p h.inspect
+  def save!
+  end
+end
+
+class UserBuilder
+  include Carpenter::ActiveRecordBuilder
+  contains :email, :first_name
+
+  def build
+    User.new
+  end
+end
+
+describe Carpenter::Cucumber::BuilderFacade do
+  describe "#build" do
+    let(:builder) { UserBuilder.new }
+    let(:facade) { Carpenter::Cucumber::BuilderFacade.new builder }
+
+    it "should build a product" do
+      builder.should_receive :build!
+      facade.build default_table
     end
+
+    it "should set attributes passed through a cucumber table" do
+      table = create_table <<-EOS
+                             | Email           | First Name |
+                             | some@gemail.com | John      |
+                           EOS
+      builder.should_receive(:with_email).with 'some@gemail.com'
+      builder.should_receive(:with_first_name).with 'John'
+
+      facade.build table
+    end
+  end
+
+  def default_table
+    create_table <<-EOS
+                   | Email           | First Name |
+                   | john@gemail.com | John      |
+                   | jill@gemail.com | Jill      |
+                 EOS
   end
 
   def create_table(table_string)
